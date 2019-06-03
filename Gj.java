dada23f16535a336486c72bb03ef32abeed51652
sumukharm <sumukharm1@gmail.com>
2019-06-03 15:25:43 -0700

    c3

--- Gj.java
+++ Gj.java
@@ -1,6 +1,6 @@
 public class Gj {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
-      System.out.println("jpp13  Jekins java");
+      System.out.println("jpp13  Jekins java Template");
    }
 }
